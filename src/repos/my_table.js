28429132fac3b480a9a08762dbba16c3e9559f0f
Nicolas Dao <nic@neap.co>
2019-10-28 17:25:37 +1100

    Update my_table.js

--- src/repos/my_table.js
+++ src/repos/my_table.js
@@ -8,6 +8,9 @@
 
 const { dynamodb: { Table } } = require('../_aws')
 
+// process.env.NUMBER_TABLE contains the DynamoDB table's name. It is wrapped in a function
+// so it can be lazy loaded. This code is supposed to work with the 'sls-config-parser' which 
+// sets environment variables after the code has been loaded.
 const TABLE = () => process.env.NUMBER_TABLE
 
 const SCHEMA = {
