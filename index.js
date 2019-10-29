3c18180585386ae412b097913c28639d7c30f0e8
Nicolas Dao <nic@neap.co>
2019-10-29 17:59:04 +1100

    Update index.js

--- index.js
+++ index.js
@@ -8,14 +8,29 @@
 
 const { my_table } = require('./src/repos')
 
+const now = new Date()
+
 my_table.insert({ device_id:1, timestamp: new Date(), hello:'world' })
 
 // Insert also supports arrays. Optionally, you can set the concurrency level for those inserts to match 
 // your provisioning set up (default is 1). 
 my_table.insert([
-	{ device_id:2, timestamp: new Date(), hello:'Nic' },
-	{ device_id:3, timestamp: new Date(), hello:'Carl' }
+	{ device_id:2, timestamp: now, hello:'Nic' },
+	{ device_id:3, timestamp: now, hello:'Carl' }
 ], { concurrency:2 })
+
+// Check if a record already exists before inserting it. 
+// IMPORTANT: 'ifNotExists' can either be a string or an array of strings. If the table is set up with a hash and a range,
+// then this option must be an array with the names of those two keys, otherwise, it won't work. If there is no range key,
+// then both a string or an array with a single item will work.
+my_table.insert({ device_id:2, timestamp:now }, { ifNotExists:['device_id', 'timestamp'] })
+	.catch(err => {
+		if (err && err.code == 'ConditionalCheckFailedException')
+			return 'This message already exists.'
+		else
+			return err.message
+	})
+	.then(console.log)
 
 my_table
 	.query('device_id').eq(1)
