bf562335c555a02f98b4c17857a0b4dc2043495a
Nicolas Dao <nic@neap.co>
2020-05-05 17:54:09 +1000

    Update my_table.js

--- src/repos/my_table.js
+++ src/repos/my_table.js
@@ -13,8 +13,16 @@ const { dynamodb: { Table } } = require('../_aws')
 // sets environment variables after the code has been loaded.
 const TABLE = () => process.env.NUMBER_TABLE
 
+// The SCHEMA allows to validate new records or updates on existing records. The types below are references 
+// to the native Javascript types plus the following custom types: 
+//	- 'id' for a value that can eitehr be a number or a string.
+//	- '[<native JS type>]' for an array.
+//	- 'date' for a Date.
+//	- '<type>!' for a required type.
+//	- null to indicates that the type can be anything or that you simply do not want to validate that type.
 const SCHEMA = {
-	device_id: 'number!',
+	device_id: 'id!',
+	count: 'number',
 	timestamp: 'date!'
 }
 
